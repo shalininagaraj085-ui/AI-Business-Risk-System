5b5d231ed088d8dcb83601c344a74e3b286ebc8a
shalininagaraj085-ui <shalininagaraj085@gmail.com>
2026-09-23 01:32:02 +1100

    Fix user model path

--- authRoutes.js
+++ authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const User = require("../models/userModel");
+const User = require("./userModel");
 
 const router = express.Router();
 
