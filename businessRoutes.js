e34ef69cb61cedba2346a1f93eb75ec3015c7030
shalininagaraj085-ui <shalininagaraj085@gmail.com>
2026-09-23 22:49:10 +1100

    Update AI API URL

--- businessRoutes.js
+++ businessRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const AI_API_URL =
     process.env.AI_API_URL ||
-    "http://127.0.0.1:8000/api/risk/analyze";
+    "https://ai-business-risk-python.onrender.com/api/risk/analyze";
 
 // =====================================================
 // AI BUSINESS COPILOT
